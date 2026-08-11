eac66ffdfe39125c0c439935e93f4fcb3f4d5bc4
Janvi Singh <janvi.singh1209@gmail.com>
2026-08-11 21:55:11 +0530

    fix InfoCard import case

--- frontend/src/pages/dashboard/HomePage.jsx
+++ frontend/src/pages/dashboard/HomePage.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import DashboardLayout from "../../components/layout/DashboardLayout";
-import InfoCard from "../../components/cards/InfoCard";
+import InfoCard from "../../components/Cards/InfoCard";
 import { API_PATHS } from "../../utils/api-path";
 import axiosInstance from "../../utils/axiosInstance";
 import useUserAuth from "../../hooks/useUserAuth"; // to protect route
